a60403a71ffe032df3ff456b46bfdb8a04908449
tienquynh1312 <160729262+tienquynh1312@users.noreply.github.com>
2024-03-03 14:47:27 +0000

    Assignment is Tricky part 1

---
+++ src/main/java/com/mycompany/app/assignment1.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+
+public class assignment1 {
+    public static void main(String[] args) {
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        n1.i = n2.i;
+        n2.i = 10;// what is n1.i?
+        System.out.println(n1.i);
+    }
+}
