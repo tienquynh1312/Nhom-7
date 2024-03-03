148da846d6239f5a961aea197af214717eda9609
phuc-vu-hong-925 <160729341+phuc-vu-hong-925@users.noreply.github.com>
2024-03-03 23:11:18 +0000

    E7

---
+++ src/main/java/com/mycompany/week1/E7.java
@@ -0,0 +1,15 @@
+package com.mycompany.week1;
+
+public class E7 {
+    public void Ex7() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if (i == 99) {
+                break;
+                // return;
+            }
+        }
+
+    }
+    
+}
