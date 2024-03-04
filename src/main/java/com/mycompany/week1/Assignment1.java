97872f66a59380d122226b7d9e938a15e5c06146
tienquynh1312 <160729262+tienquynh1312@users.noreply.github.com>
2024-03-04 06:35:20 +0000

    Assignment1.java

---
+++ src/main/java/com/mycompany/week1/Assignment1.java
@@ -0,0 +1,13 @@
+package com.mycompany.week1;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        n1.i = n2.i;
+        n2.i = 10;
+        System.out.println(n1.i);
+    }
+}
