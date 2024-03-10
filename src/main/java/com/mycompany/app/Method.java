bd202d5171b411b9f336619cbd8bf7409e0613a4
tienquynh1312 <160729262+tienquynh1312@users.noreply.github.com>
2024-03-10 15:05:49 +0000

    Method.java

---
+++ src/main/java/com/mycompany/app/Method.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+public class Method {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+      }
+    
+      public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+      }
+}
